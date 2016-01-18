22a82ebf7943d0489c4292ceb1eb2f604d37dda4
Nolan Lawson <nolan.lawson@gmail.com>
2016-01-17 20:02:08 -0500

    (#3) - fix IE8 support, test in IE 8

--- test/xhr-shim.js
+++ test/xhr-shim.js
@@ -13,13 +13,18 @@ XHR.prototype.open = function (type, script) {
 XHR.prototype.send = function () {
   var that = this;
   process.nextTick(function () {
-    if (fs.existsSync(that.script)) {
-      that.responseText = fs.readFileSync(that.script, 'utf-8');
-      that.status = 200;
-    } else {
-      that.status = 404;
-    }
-    that.onload();
+    that.readyState = 2;
+    that.onreadystatechange();
+    process.nextTick(function () {
+      that.readyState = 4;
+      if (fs.existsSync(that.script)) {
+        that.responseText = fs.readFileSync(that.script, 'utf-8');
+        that.status = 200;
+      } else {
+        that.status = 404;
+      }
+      that.onreadystatechange();
+    });
   });
 };
 
